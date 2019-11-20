7e13a3762abfd720f91863bc5a4bafc6cbefe4fc
Angel Hurtado <angelxehg@outlook.com>
2019-11-19 20:39:55 -0600

    Create Classes

---
+++ src/calculathor/Term.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculathor;
+
+/**
+ *
+ * @author angel
+ */
+public class Term {
+
+    public Term(String newTerm) {
+        // TODO: Create from String
+    }
+
+}
